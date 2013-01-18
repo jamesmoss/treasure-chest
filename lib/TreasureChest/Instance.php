1950138f2e3cae310b1123da8557e6798f83b0ff
Joe Holdcroft <joe@message.co.uk>
2013-01-18 14:29:58 +0000

    Destroying magic methods & introducing some tests

--- lib/TreasureChest/Instance.php
+++ lib/TreasureChest/Instance.php
@@ -7,73 +7,57 @@
  * This gives the ability to logically group a set of keys and invalidate
  * them all at once making cache management easier.
  * Note: This library requires APC version 3.1.4 or higher.
- * 
+ *
  * @author James Moss <email@jamesmoss.co.uk>
- * @version 0.2
- * @package namespace-cache
+ * @version 0.1.4
+ * @package treasure-chest
  */
- 
- 
+
+
 namespace TreasureChest;
- 
-class Instance
-{	
+
+class Instance implements CacheInterface
+{
 	/**
 	 * The character used to indicate the seperation of cache namespaces
 	 *
-	 * @var string 
+	 * @var string
 	 */
 	protected $delimiter = ':';
 
 	/**
-	 * This namespace can be appended to the start of all other keys passed in to 
+	 * This namespace can be appended to the start of all other keys passed in to
 	 * the class to faciliate logical partitioning of cache data.
 	 *
-	 * @var string 
+	 * @var string
 	 */
 	protected $prefix = '';
-	
-	public function __construct($cache)
+
+	/**
+	 * Constructor.
+	 *
+	 * @param CacheInterface $cache The cache instance to use
+	 */
+	public function __construct(CacheInterface $cache)
 	{
-		$this->cache = $cache;
+		$this->cache  = $cache;
 		$this->mapper = new KeyMapper($this->cache, $this->delimiter);
 	}
-	
-	public function __call($method, $args)
-	{
-		// Only the methods from the CacheInterface are allowed to be called
-		$allowedMethods = get_class_methods('\\'.__NAMESPACE__.'\\CacheInterface');
-		
-		if(!in_array($method, $allowedMethods)) {
-			throw new Exception('Unknown method '.$method);
-		}
 
-		// If we are auto prefixing all keys with a namespace, do that here
-		if($this->prefix) {
-			$args[0] = $this->prefix.$this->delimiter.$args[0];
-		}
-		
-		// convert the user supplied key into one actually used in the cache
-		$args[0] = $this->mapper->parse($args[0]);
-		
-		// Call the method on the cache class, passing in supplied arguments
-		return call_user_func_array(array($this->cache, $method), $args);
-	}
-	
 	/**
 	 * Sets the current version of the provided namespace
 	 *
-	 * @param string $prefix The new prefix to use 
+	 * @param string $prefix The new prefix to use
 	 */
 	public function setPrefix($prefix)
 	{
 		$this->prefix = $prefix;
 	}
-	
+
 	/**
 	 * Sets the namespace delimiter
 	 *
-	 * @param string $prefix The new delimiter to use 
+	 * @param string $prefix The new delimiter to use
 	 */
 	public function setDelimiter($delimiter)
 	{
@@ -83,7 +67,7 @@ class Instance
 
 	/**
 	 * Set your own custom mapper.
-	 * 
+	 *
 	 * @param KeyMapperInterface $mapper [description]
 	 */
 	public function setMapper(KeyMapperInterface $mapper)
@@ -91,7 +75,52 @@ class Instance
 		$this->mapper = $mapper;
 		$this->mapper->setDelimiter($this->delimiter);
 	}
-	
+
+	public function add($key, $var = null, $ttl = 0)
+	{
+		return $this->_callCacheMethod('add', func_get_args());
+	}
+
+	public function store($key, $var = null, $ttl = 0)
+	{
+		return $this->_callCacheMethod('store', func_get_args());
+	}
+
+	public function replace($key, $var = null, $ttl = 0)
+	{
+		return $this->_callCacheMethod('replace', func_get_args());
+	}
+
+	public function exists($key)
+	{
+		return $this->_callCacheMethod('exists', func_get_args());
+	}
+
+	public function fetch($key, &$success = false)
+	{
+		return $this->_callCacheMethod('fetch', func_get_args());
+	}
+
+	public function inc($key, $step = 1, &$success = null)
+	{
+		return $this->_callCacheMethod('inc', func_get_args());
+	}
+
+	public function dec($key, $step = 1, &$success = null)
+	{
+		return $this->_callCacheMethod('dec', func_get_args());
+	}
+
+	public function delete($key)
+	{
+		return $this->_callCacheMethod('delete', func_get_args());
+	}
+
+	public function clear()
+	{
+		return $this->_callCacheMethod('clear', func_get_args());
+	}
+
 	/**
 	 * Deletes all the keys in an entire namespace.
 	 *
@@ -104,5 +133,33 @@ class Instance
 		}
 
 		return $this->mapper->invalidate($namespace);
+	}
+
+	/**
+	 * Call a given method on the stored cache instance.
+	 *
+	 * This is used with specific methods, rather than using the `__call()`
+	 * magic method so that we can use an interface to define the methods. This
+	 * makes this class easier to hint in your application.
+	 *
+	 * @param  string $method Method name to call
+	 * @param  array  $args   Arguments to use
+	 *
+	 * @return mixed          Result of calling the method on the cache instance
+	 */
+	protected function _callCacheMethod($method, array $args = array())
+	{
+		if (isset($args[0])) {
+			// If we are auto prefixing all keys with a namespace, do that here
+			if($this->prefix) {
+				$args[0] = $this->prefix.$this->delimiter.$args[0];
+			}
+
+			// Convert the user supplied key into one actually used in the cache
+			$args[0] = $this->mapper->parse($args[0]);
+		}
+
+		// Call the method on the cache class, passing in supplied arguments
+		return call_user_func_array(array($this->cache, $method), $args);
 	}
 }
